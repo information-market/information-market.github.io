756358074a7394438086119f42717f4f915be05c
information-market <himeldev+@gmail.com>
2018-06-04 00:22:45 -0700

    Fixing WWW page

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(function () {
+        $(".demo1").bootstrapNews({
+            newsPerPage: 3,
+            autoplay: true,
+			pauseOnHover:true,
+            direction: 'up',
+            newsTickerInterval: 4000,
+            onToDo: function () {
+                //console.log(this);
+            }
+        });
